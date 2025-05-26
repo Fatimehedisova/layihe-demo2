628deca1f26f89f1e05b202342a8dabdebc41b3f
FatimeHedisova <fatimehedisova14@gmail.com>
2025-05-27 03:00:29 +0400

    header-changes-demo-5

--- homeapp.js
+++ homeapp.js
@@ -1,8 +1,3 @@
-fetch('./header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header-placeholder').innerHTML = data;
-    });
 fetch("./products.json")
     .then(res => res.json())
     .then(data => {
